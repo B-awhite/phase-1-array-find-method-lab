9e638823531bcea3e00f5a60fd317541b9395a4e
Bria White <bria.awhite@gmail.com>
2022-04-14 23:49:53 -0400

    commit tree e64aa1daab0bfdea16260e2efd2d9d489038ac7b

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+//code your solution here
+const superbowlWin = (record) => {
+    let result = record.find(record => record.result === "W")
+    return !!result ? result.year : undefined 
+}
+
+// const superbowlWin = (record) => {
+//     let result = record.find(record => record.result === "W")
+//     if(result === true){
+//         return result.year
+//     }
+// }
+
